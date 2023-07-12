ede3499563bec26548d1ba60c1a91ee99fa06fa9
Meriam <132971614+msadio8@users.noreply.github.com>
2023-07-12 18:20:50 +1000

    added functions

--- assets/css/script.js
+++ assets/css/script.js
@@ -130,4 +130,24 @@ function initPage() {
       });
     });
   }
+
+  // Get history from local storage if any
+  searchEl.addEventListener("click", function () {
+    const searchTerm = cityEl.value;
+    getWeather(searchTerm);
+    searchHistory.push(searchTerm);
+    localStorage.setItem("search", JSON.stringify(searchHistory));
+    renderSearchHistory();
+  });
+
+  // Clear history button
+  clearEl.addEventListener("click", function () {
+    localStorage.clear();
+    searchHistory = [];
+    renderSearchHistory();
+  });
+
+  function k2f(K) {
+    return Math.floor((K - 273.15) * 1.8 + 32);
+  }
 }
