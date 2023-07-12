ff65dbe47ab7eaa3c7295e0ab19026e86644399f
Meriam <132971614+msadio8@users.noreply.github.com>
2023-07-12 18:01:47 +1000

    add Js code

---
+++ assets/css/script.js
@@ -0,0 +1,18 @@
+function initPage() {
+  const cityEl = document.getElementById("enter-city");
+  const searchEl = document.getElementById("search-button");
+  const clearEl = document.getElementById("clear-history");
+  const nameEl = document.getElementById("city-name");
+  const currentPicEl = document.getElementById("current-pic");
+  const currentTempEl = document.getElementById("temperature");
+  const currentHumidityEl = document.getElementById("humidity");
+  const currentWindEl = document.getElementById("wind-speed");
+  const currentUVEl = document.getElementById("UV-index");
+  const historyEl = document.getElementById("history");
+  var fivedayEl = document.getElementById("fiveday-header");
+  var todayweatherEl = document.getElementById("today-weather");
+  let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+
+  // API Key Obtain from Openweathermap
+  const APIKey = "b3d932933f0a2a3f468a7d000ac87fed";
+}
